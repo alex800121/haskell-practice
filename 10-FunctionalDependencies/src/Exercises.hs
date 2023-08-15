afd7c72a1abc94356bb84e18ba53821e47cff67b
alex800121 <alex800121@hotmail.com>
2023-08-15 19:06:06 +0800

    lack prime

--- 10-FunctionalDependencies/src/Exercises.hs
+++ 10-FunctionalDependencies/src/Exercises.hs
@@ -5,46 +5,39 @@
 {-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
-{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE RankNTypes #-}
+
 module Exercises where
 
-import Data.Kind (Type, Constraint)
-import GHC.TypeLits (Symbol, TypeError, ErrorMessage(..))
+import Data.Kind (Constraint, Type)
+import Data.Void (Void)
 import GHC.Generics (Generic (..))
 import qualified GHC.Generics as G
-import Data.Void (Void)
-
-
-
-
+import GHC.TypeLits (ErrorMessage (..), Symbol, TypeError)
 
 {- ONE -}
 
 -- | Recall an old friend, the 'Newtype' class:
-
-class Newtype (new :: Type) (old :: Type) | new -> old where
-  wrap   :: old -> new
+class old ~ Old new => Newtype (new :: Type) (old :: Type) where
+  type Old new
+  wrap :: old -> new
   unwrap :: new -> old
 
 -- | a. Can we add a functional dependency to this class?
 
 -- | b. Why can't we add two?
 
-
-
-
-
 {- TWO -}
 
 -- | Let's go back to a problem we had in the last exercise, and imagine a very
 -- simple cache in IO. Uncomment the following:
-
 class CanCache (entity :: Type) (index :: Type) (m :: Type -> Type) | entity -> index where
   store :: entity -> m ()
-  load  :: index -> m (Maybe entity)
+  load :: index -> m (Maybe entity)
 
 -- | a. Uh oh - there's already a problem! Any @entity@ type should have a
 -- fixed type of id/@index@, though... if only we could convince GHC... Could
@@ -56,10 +49,6 @@ class CanCache (entity :: Type) (index :: Type) (m :: Type -> Type) | entity -> 
 -- | c. Is there any sort of functional dependency that relates our
 -- parameterised functor to @entity@ or @index@? If so, how? If not, why not?
 
-
-
-
-
 {- THREE -}
 
 -- | Let's re-introduce one of our old favourites:
@@ -68,11 +57,10 @@ data Nat = Z | S Nat
 -- | When we did our chapter on @TypeFamilies@, we wrote an @Add@ family to add
 -- two type-level naturals together. If we do a side-by-side comparison of the
 -- equivalent "class-based" approach:
-
-class       Add  (x :: Nat) (y :: Nat) (z :: Nat) | x y -> z where
+class Add (x :: Nat) (y :: Nat) (z :: Nat) | x y -> z where
   append :: Vec x a -> Vec y a -> Vec z a
 
-type family Add' (x :: Nat) (y :: Nat)    :: Nat
+type family Add' (x :: Nat) (y :: Nat) :: Nat
 
 -- | We see here that there are parallels between classes and type families.
 -- Type families produce a result, not a constraint, though we could write
@@ -83,11 +71,10 @@ type family Add' (x :: Nat) (y :: Nat)    :: Nat
 -- | a. Write the two required instances for the 'Add' class by
 -- pattern-matching on the first argument. Remember that instances can have
 -- constraints, and this is how we do recursion!
-
 instance Add 'Z y y where
   append VNil ys = ys
 
-instance Add x y z => Add ('S x) y ('S z) where
+instance (Add x y z) => Add ('S x) y ('S z) where
   append (VCons x xs) ys = VCons x (append xs ys)
 
 class Add'' (x :: Nat) (y :: Nat) where
@@ -98,7 +85,7 @@ instance Add'' 'Z y where
   type AddResult 'Z y = y
   append' VNil ys = ys
 
-instance Add'' x y => Add'' ('S x) y where
+instance (Add'' x y) => Add'' ('S x) y where
   type AddResult ('S x) y = 'S (AddResult x y)
   append' (VCons x xs) ys = VCons x (append' xs ys)
 
@@ -110,18 +97,16 @@ type family All (c :: k -> Constraint) (xs :: [k]) :: Constraint where
   All _ '[] = ()
   All c (x ': xs) = (c x, All c xs)
 
-deriving instance Show a => Show (Vec n a)
+deriving instance (Show a) => Show (Vec n a)
+
 -- | b. By our analogy, a type family has only "one functional dependency" -
 -- all its inputs to its one output. Can we write _more_ functional
--- dependencies for @Add@? Aside from @x y -> z@? 
+-- dependencies for @Add@? Aside from @x y -> z@?
 
 -- | c. We know with addition, @x + y = z@ implies @y + x = z@ and @z - x = y@.
 -- This should mean that any pair of these three variables should determine the
 -- other! Why couldn't we write all the possible functional dependencies that
 -- /should/ make sense?
-
-
-
 
 {- FOUR -}
 
@@ -130,18 +115,18 @@ data Proxy (a :: k) = Proxy
 -- | As we all know, type signatures are /not/ documentation. This is really
 -- because the names of types are far too confusing. To that end, we can give
 -- our types friendlier names to make the coding experience less intimidating:
-
 class (x :: k) `IsNamed` (label :: Symbol) | x -> label, label -> x where
-  fromName :: Proxy x     -> Proxy label
+  fromName :: Proxy x -> Proxy label
   fromName _ = Proxy
 
   toName :: Proxy label -> Proxy x
   toName _ = Proxy
 
 -- | Now we have this class, we can get to work!
+instance Int `IsNamed` "Dylan"
 
-instance Int   `IsNamed` "Dylan"
-instance IO    `IsNamed` "Barbara"
+instance IO `IsNamed` "Barbara"
+
 instance Float `IsNamed` "Kenneth"
 
 -- | a. In our glorious new utopia, we decide to enact a law that says, "No two
@@ -149,16 +134,11 @@ instance Float `IsNamed` "Kenneth"
 -- Is there a way to get GHC to help us uphold the law?
 
 -- | b. Write the identity function restricted to types named "Kenneth".
-
-idKenneth :: a `IsNamed` "Kenneth" => a -> a
+idKenneth :: (a `IsNamed` "Kenneth") => a -> a
 idKenneth = id
 
 -- | c. Can you think of a less-contrived reason why labelling certain types
 -- might be useful in real-world code?
-
-
-
-
 
 {- FIVE -}
 
@@ -178,25 +158,19 @@ instance Omnipresent "Tom!" 'Z
 
 -- | c. Add another similarly-omnipresent parameter to this type class.
 
-
-
-
-
 {- SIX -}
 
 -- | You knew it was coming, didn't you?
-
 data HList (xs :: [Type]) where
-  HNil  :: HList '[]
+  HNil :: HList '[]
   HCons :: x -> HList xs -> HList (x ': xs)
 
 data SNat (n :: Nat) where
-  SZ ::           SNat  'Z
+  SZ :: SNat 'Z
   SS :: SNat n -> SNat ('S n)
 
 -- | a. Write a function (probably in a class) that takes an 'SNat' and an
 -- 'HList', and returns the value at the 'SNat''s index within the 'HList'.
-
 class Index (n :: Nat) (xs :: [Type]) where
   type At n xs :: Type
   index :: SNat n -> HList xs -> At n xs
@@ -205,11 +179,11 @@ instance Index 'Z (x ': xs) where
   type At 'Z (x ': xs) = x
   index _ (HCons x _) = x
 
-instance Index n xs =>  Index ('S n) (y ': xs) where
+instance (Index n xs) => Index ('S n) (y ': xs) where
   type At ('S n) (y ': xs) = At n xs
   index (SS n) (HCons _ ys) = index n ys
 
-instance TypeError (Text "Out of index") => Index n '[] where
+instance (TypeError (Text "Out of index")) => Index n '[] where
   type At n '[] = Void
   index = error "unreachable"
 
@@ -218,34 +192,54 @@ instance TypeError (Text "Out of index") => Index n '[] where
 -- | c. Write a custom type error!
 
 -- | d. Implement 'take' for the 'HList'.
+class Take (n :: Nat) (xs :: [Type]) where
+  type Taken n xs :: [Type]
+  take :: SNat n -> HList xs -> HList (Taken n xs)
 
+instance Take 'Z xs where
+  type Taken 'Z xs = '[]
+  take _ _ = HNil
 
+instance Take n '[] where
+  type Taken n '[] = '[]
+  take _ _ = HNil
 
-
+instance (Take n xs) => Take ('S n) (x ': xs) where
+  type Taken ('S n) (x ': xs) = x ': Taken n xs
+  take (SS n) (HCons x xs) = HCons x (Exercises.take n xs)
 
 {- SEVEN -}
 
 -- | Recall our variant type:
-
-deriving instance All Show xs => Show (Variant xs)
+deriving instance (All Show xs) => Show (Variant xs)
 
 data Variant (xs :: [Type]) where
-  Here  ::         x  -> Variant (x ': xs)
+  Here :: x -> Variant (x ': xs)
   There :: Variant xs -> Variant (y ': xs)
 
 -- | We previously wrote a function to "inject" a value into a variant:
+type family Project x xs where
+  Project x '[] = '[]
+  Project x (x ': xs) = xs
+  Project x (y ': xs) = y ': Project x xs
 
-class Inject (x :: Type) (xs :: [Type]) where
-  type Project xs :: [Type]
+class (ys ~ Project x xs) => Inject (x :: Type) (xs :: [Type]) (ys :: [Type]) where
   inject :: x -> Variant xs
+  project :: Proxy x -> Variant xs -> Either x (Variant ys)
 
-instance {-# OVERLAPPING #-} Inject x (x ': xs) where
-  type Project (x ': xs) = xs
+instance {-# OVERLAPPING #-} (Project x (x ': xs) ~ xs) => Inject x (x ': xs) xs where
   inject = Here
+  project _ (Here x) = Left x
+  project _ (There xs) = Right xs
 
-instance {-# INCOHERENT #-} Inject x xs => Inject x (y ': xs) where
-  type Project (y ': xs) = y ': Project xs
+instance (Project x (y ': xs) ~ y ': Project x xs, Inject x xs ys) => Inject x (y ': xs) (y ': ys) where
   inject = There . inject
+  project _ (Here x) = Right (inject x)
+  project p (There xs) = There <$> project p xs
+
+instance (TypeError (Text "The given type \"" :<>: ShowType x :<>: Text "\" is not in the Variant")) => Inject x '[] '[] where
+  inject = error "unreachable"
+  project = error "unreachable"
 
 -- | Write a function to "project" a value /out of/ a variant. In other words,
 -- I would like a function that takes a proxy of a type, a variant containing
@@ -256,10 +250,6 @@ instance {-# INCOHERENT #-} Inject x xs => Inject x (y ': xs) where
 --   project (Proxy :: Proxy Bool) (inject True :: Variant '[Int, String, Bool])
 --     === Left Bool :: Either Bool (Variant '[Int, String])
 -- @
-
-
-
-
 
 {- EIGHT -}
 
@@ -273,10 +263,19 @@ instance {-# INCOHERENT #-} Inject x xs => Inject x (y ': xs) where
 
 -- | Write the type class required to implement this function, along with all
 -- its instances and functional dependencies.
+deriving instance (All Show xs) => Show (HList xs)
 
+class Update (n :: Nat) (x :: Type) (y :: Type) (xs :: [Type]) where
+  type Updated n x y xs :: [Type]
+  update :: SNat n -> (x -> y) -> HList xs -> HList (Updated n x y xs)
 
+instance (x ~ z) => Update 'Z x y (z ': xs) where
+  type Updated 'Z x y (z ': xs) = y ': xs
+  update _ f (HCons x xs) = HCons (f x) xs
 
-
+instance (Update n x y xs) => Update ('S n) x y (z ': xs) where
+  type Updated ('S n) x y (z ': xs) = z ': Updated n x y xs
+  update (SS n) f (HCons z xs) = HCons z (update n f xs)
 
 {- NINE -}
 
@@ -287,20 +286,17 @@ instance {-# INCOHERENT #-} Inject x xs => Inject x (y ': xs) where
 
 -- | We can write a little function to get the name of a type as a type-level
 -- symbol like so:
-
 class NameOf (x :: Type) (name :: Symbol) | x -> name
-instance GNameOf (Rep x) name => NameOf x name
+
+instance (GNameOf (Rep x) name) => NameOf x name
 
 -- | We then have to implement this class that examines the generic tree...
 class GNameOf (rep :: Type -> Type) (name :: Symbol) | rep -> name
+
 instance GNameOf (G.D1 ('G.MetaData name a b c) d) name
 
 -- | Write a function to get the names of the constructors of a type as a
 -- type-level list of symbols.
-
-
-
-
 
 {- TEN -}
 
@@ -310,11 +306,11 @@ instance GNameOf (G.D1 ('G.MetaData name a b c) d) name
 --
 -- liftA1 :: Applicative f => (a -> b) -> f a -> f b
 -- liftA1 = lift
--- 
+--
 -- liftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 -- liftA2 = lift
 --
--- 
+--
 -- liftA3 :: Applicative f => (a -> b -> c -> d) -> f a -> f b -> f c -> f d
 -- liftA3 = lift
 
@@ -332,3 +328,25 @@ instance GNameOf (G.D1 ('G.MetaData name a b c) d) name
 --
 -- >>> :t lift (++)
 -- lift (++) :: Applicative f => f [a] -> f [a] -> f [a]
+
+type family GetApplicative (a :: Type) :: Type -> Type where
+  GetApplicative (f a -> o) = GetApplicative o
+  GetApplicative (f a) = f
+
+type family GetOutcome f i where
+  GetOutcome f (a -> b) = f a -> GetOutcome f b
+  GetOutcome f a = f a
+
+class (Applicative f, f ~ GetApplicative o, o ~ GetOutcome f i) => Lift f i o where
+  lift' :: f i -> o
+
+-- instance (Applicative f, Lift f b o, o' ~ (f a -> o)) => Lift f (a -> b) o' where
+instance {-# OVERLAPPING #-} (Applicative f, Lift f b o, o' ~ (f a -> o)) => Lift f (a -> b) o' where
+  lift' fs xs = lift' (fs <*> xs)
+
+-- instance {-# INCOHERENT #-} (Applicative f, o ~ f a, GetApplicative (f a) ~ f, GetOutcome f a ~ f a) => Lift f a o where
+instance (Applicative f, o ~ f a, GetApplicative (f a) ~ f, GetOutcome f a ~ f a) => Lift f a o where
+  lift' = id
+
+lift :: (Applicative f, Lift f i o) => i -> o
+lift = lift' . pure
