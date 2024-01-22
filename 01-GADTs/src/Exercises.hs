45e036ff8cd4a2c3b3ed05f93c2db3fd28659499
alex800121 <alex800121@hotmail.com>
2024-01-23 08:07:38 +0900

    practice3

--- 01-GADTs/src/Exercises.hs
+++ 01-GADTs/src/Exercises.hs
@@ -1,175 +1,181 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Exercises where
 
-
-
-
+import Data.Kind (Type)
 
 {- ONE -}
 
 -- | Let's introduce a new class, 'Countable', and some instances to match.
 class Countable a where count :: a -> Int
-instance Countable Int  where count   = id
-instance Countable [a]  where count   = length
+
+instance Countable Int where count = id
+
+instance Countable [a] where count = length
+
 instance Countable Bool where count x = if x then 1 else 0
 
 -- | a. Build a GADT, 'CountableList', that can hold a list of 'Countable'
 -- things.
-
 data CountableList where
-  -- ...
-
+  CNil :: CountableList
+  CCons :: (Countable a) => a -> CountableList -> CountableList
 
 -- | b. Write a function that takes the sum of all members of a 'CountableList'
 -- once they have been 'count'ed.
-
 countList :: CountableList -> Int
-countList = error "Implement me!"
-
+countList CNil = 0
+countList (CCons x xs) = count x + countList xs
 
 -- | c. Write a function that removes all elements whose count is 0.
-
 dropZero :: CountableList -> CountableList
-dropZero = error "Implement me!"
-
+dropZero CNil = CNil
+dropZero (CCons x xs) = if count x == 0 then xs else CCons x (dropZero xs)
 
 -- | d. Can we write a function that removes all the things in the list of type
 -- 'Int'? If not, why not?
-
 filterInts :: CountableList -> CountableList
 filterInts = error "Contemplate me!"
-
-
-
-
 
 {- TWO -}
 
 -- | a. Write a list that can take /any/ type, without any constraints.
-
 data AnyList where
-  -- ...
+  AnyNil :: AnyList
+  AnyCons :: a -> AnyList -> AnyList
 
 -- | b. How many of the following functions can we implement for an 'AnyList'?
-
 reverseAnyList :: AnyList -> AnyList
-reverseAnyList = undefined
+reverseAnyList = go AnyNil
+  where
+    go acc AnyNil = acc
+    go acc (AnyCons x xs) = go (AnyCons x acc) xs
 
 filterAnyList :: (a -> Bool) -> AnyList -> AnyList
 filterAnyList = undefined
 
 lengthAnyList :: AnyList -> Int
-lengthAnyList = undefined
+lengthAnyList AnyNil = 0
+lengthAnyList (AnyCons _ xs) = 1 + lengthAnyList xs
 
-foldAnyList :: Monoid m => AnyList -> m
+foldAnyList :: (Monoid m) => AnyList -> m
 foldAnyList = undefined
 
 isEmptyAnyList :: AnyList -> Bool
-isEmptyAnyList = undefined
+isEmptyAnyList AnyNil = True
+isEmptyAnyList _ = False
 
 instance Show AnyList where
-  show = error "What about me?"
-
-
-
-
+  show AnyNil = "[]"
+  show (AnyCons x xs) = "[_" ++ show1 xs
+    where
+      show1 AnyNil = "]"
+      show1 (AnyCons _ xs) = ",_" ++ show1 xs
 
 {- THREE -}
 
 -- | Consider the following GADT:
-
 data TransformableTo output where
-  TransformWith
-    :: (input -> output)
-    ->  input
-    -> TransformableTo output
+  TransformWith ::
+    (input -> output) ->
+    input ->
+    TransformableTo output
 
 -- | ... and the following values of this GADT:
-
 transformable1 :: TransformableTo String
 transformable1 = TransformWith show 2.5
 
 transformable2 :: TransformableTo String
 transformable2 = TransformWith (uncurry (++)) ("Hello,", " world!")
 
+-- >>> transformable1
+-- No instance for `Show (TransformableTo String)'
+--   arising from a use of `evalPrint'
+-- In a stmt of an interactive GHCi command: evalPrint it_aetW
+
 -- | a. Which type variable is existential inside 'TransformableTo'? What is
 -- the only thing we can do to it?
 
 -- | b. Could we write an 'Eq' instance for 'TransformableTo'? What would we be
 -- able to check?
+instance (Eq output) => Eq (TransformableTo output) where
+  TransformWith f0 i0 == TransformWith f1 i1 = f0 i0 == f1 i1
 
 -- | c. Could we write a 'Functor' instance for 'TransformableTo'? If so, write
 -- it. If not, why not?
 
-
-
-
-
 {- FOUR -}
 
 -- | Here's another GADT:
-
 data EqPair where
-  EqPair :: Eq a => a -> a -> EqPair
+  EqPair :: (Eq a) => a -> a -> EqPair
 
 -- | a. There's one (maybe two) useful function to write for 'EqPair'; what is
 -- it?
+eqPair (EqPair x y) = x == y
 
 -- | b. How could we change the type so that @a@ is not existential? (Don't
 -- overthink it!)
+data EqPair' a where
+  EqPair' :: (Eq a) => a -> a -> EqPair' a
 
 -- | c. If we made the change that was suggested in (b), would we still need a
 -- GADT? Or could we now represent our type as an ADT?
-
-
-
-
+data EqPair'' a = (Eq a) => EqPair'' a a
 
 {- FIVE -}
 
 -- | Perhaps a slightly less intuitive feature of GADTs is that we can set our
 -- type parameters (in this case @a@) to different types depending on the
 -- constructor.
-
 data MysteryBox a where
-  EmptyBox  ::                                MysteryBox ()
-  IntBox    :: Int    -> MysteryBox ()     -> MysteryBox Int
-  StringBox :: String -> MysteryBox Int    -> MysteryBox String
-  BoolBox   :: Bool   -> MysteryBox String -> MysteryBox Bool
+  EmptyBox :: MysteryBox ()
+  IntBox :: Int -> MysteryBox () -> MysteryBox Int
+  StringBox :: String -> MysteryBox Int -> MysteryBox String
+  BoolBox :: Bool -> MysteryBox String -> MysteryBox Bool
 
 -- | When we pattern-match, the type-checker is clever enough to
 -- restrict the branches we have to check to the ones that could produce
 -- something of the given type.
-
 getInt :: MysteryBox Int -> Int
 getInt (IntBox int _) = int
 
 -- | a. Implement the following function by returning a value directly from a
 -- pattern-match:
-
 getInt' :: MysteryBox String -> Int
-getInt' _doSomeCleverPatternMatching = error "Return that value"
+getInt' (StringBox _ m) = getInt m
 
 -- | b. Write the following function. Again, don't overthink it!
-
 countLayers :: MysteryBox a -> Int
-countLayers = error "Implement me"
+countLayers EmptyBox = 0
+countLayers (IntBox _ i) = 1 + countLayers i
+countLayers (StringBox _ i) = 1 + countLayers i
+countLayers (BoolBox _ i) = 1 + countLayers i
 
 -- | c. Try to implement a function that removes one layer of "Box". For
 -- example, this should turn a BoolBox into a StringBox, and so on. What gets
 -- in our way? What would its type be?
+type family InnerLayer a :: Type where
+  InnerLayer Int = ()
+  InnerLayer String = Int
+  InnerLayer Bool = String
 
-
-
-
+innerLayer :: MysteryBox a -> MysteryBox (InnerLayer a)
+innerLayer (IntBox _ i) = i
+innerLayer (StringBox _ i) = i
+innerLayer (BoolBox _ i) = i
 
 {- SIX -}
 
 -- | We can even use our type parameters to keep track of the types inside an
 -- 'HList'!  For example, this heterogeneous list contains no existentials:
-
 data HList a where
-  HNil  :: HList ()
+  HNil :: HList ()
   HCons :: head -> HList tail -> HList (head, tail)
 
 exampleHList :: HList (String, (Int, (Bool, ())))
@@ -179,46 +185,55 @@ exampleHList = HCons "Tom" (HCons 25 (HCons True HNil))
 -- should be /safe/: you can use the type signature to tell GHC that you won't
 -- need to pattern-match on HNil, and therefore the return type shouldn't be
 -- wrapped in a 'Maybe'!
+head :: HList (a, b) -> a
+head (HCons x _) = x
 
 -- | b. Currently, the tuples are nested. Can you pattern-match on something of
 -- type @HList (Int, String, Bool, ())@? Which constructor would work?
-
 patternMatchMe :: HList (Int, String, Bool, ()) -> Int
 patternMatchMe = undefined
 
 -- | c. Can you write a function that appends one 'HList' to the end of
 -- another? What problems do you run into?
+type family a ++ b where
+  () ++ ys = ys
+  (x, xs) ++ ys = (x, xs ++ ys)
 
-
-
-
+append :: HList a -> HList b -> HList (a ++ b)
+append HNil ys = ys
+append (HCons x xs) ys = HCons x (append xs ys)
 
 {- SEVEN -}
 
 -- | Here are two data types that may help:
-
 data Empty
+
 data Branch left centre right
 
 -- | a. Using these, and the outline for 'HList' above, build a heterogeneous
 -- /tree/. None of the variables should be existential.
-
 data HTree a where
-  -- ...
+  HLeaf :: HTree Empty
+  HBranch :: HTree left -> centre -> HTree right -> HTree (Branch left centre right)
+
+-- ...
 
 -- | b. Implement a function that deletes the left subtree. The type should be
 -- strong enough that GHC will do most of the work for you. Once you have it,
 -- try breaking the implementation - does it type-check? If not, why not?
+pruneLeft :: HTree (Branch l c r) -> HTree (Branch Empty c r)
+pruneLeft (HBranch l c r) = HBranch HLeaf c r
 
 -- | c. Implement 'Eq' for 'HTree's. Note that you might have to write more
 -- than one to cover all possible HTrees. You might also need an extension or
 -- two, so look out for something... flexible... in the error messages!
 -- Recursion is your friend here - you shouldn't need to add a constraint to
 -- the GADT!
+instance Eq (HTree Empty) where
+  _ == _ = True
 
-
-
-
+instance (Eq (HTree l), Eq c, Eq (HTree r)) => Eq (HTree (Branch l c r)) where
+  HBranch l0 c0 r0 == HBranch l1 c1 r1 = c0 == c1 && l0 == l1 && r0 == r1
 
 {- EIGHT -}
 
@@ -229,67 +244,74 @@ data HTree a where
 --   f :: AlternatingList Bool Int
 --   f = ACons True (ACons 1 (ACons False (ACons 2 ANil)))
 -- @
-
 data AlternatingList a b where
-  -- ...
+  ANil :: AlternatingList a b
+  ACons :: a -> AlternatingList b a -> AlternatingList a b
+
+-- ...
 
 -- | b. Implement the following functions.
-
 getFirsts :: AlternatingList a b -> [a]
-getFirsts = error "Implement me!"
+getFirsts ANil = []
+getFirsts (ACons x xs) = x : getSeconds xs
 
 getSeconds :: AlternatingList a b -> [b]
-getSeconds = error "Implement me, too!"
+getSeconds ANil = []
+getSeconds (ACons _ xs) = getFirsts xs
 
 -- | c. One more for luck: write this one using the above two functions, and
 -- then write it such that it only does a single pass over the list.
-
 foldValues :: (Monoid a, Monoid b) => AlternatingList a b -> (a, b)
-foldValues = error "Implement me, three!"
-
-
-
-
+foldValues ANil = (mempty, mempty)
+foldValues (ACons x xs) = let (b, a) = foldValues xs in (x <> a, b)
 
 {- NINE -}
 
 -- | Here's the "classic" example of a GADT, in which we build a simple
 -- expression language. Note that we use the type parameter to make sure that
 -- our expression is well-formed.
-
 data Expr a where
-  Equals    :: Expr Int  -> Expr Int            -> Expr Bool
-  Add       :: Expr Int  -> Expr Int            -> Expr Int
-  If        :: Expr Bool -> Expr a   -> Expr a  -> Expr a
-  IntValue  :: Int                              -> Expr Int
-  BoolValue :: Bool                             -> Expr Bool
+  Equals :: Expr Int -> Expr Int -> Expr Bool
+  Add :: Expr Int -> Expr Int -> Expr Int
+  If :: Expr Bool -> Expr a -> Expr a -> Expr a
+  IntValue :: Int -> Expr Int
+  BoolValue :: Bool -> Expr Bool
 
 -- | a. Implement the following function and marvel at the typechecker:
-
 eval :: Expr a -> a
-eval = error "Implement me"
+eval (BoolValue b) = b
+eval (IntValue i) = i
+eval (If b x y) = if eval b then eval x else eval y
+eval (Add x y) = eval x + eval y
+eval (Equals x y) = eval x == eval y
 
 -- | b. Here's an "untyped" expression language. Implement a parser from this
 -- into our well-typed language. Note that (until we cover higher-rank
 -- polymorphism) we have to fix the return type. Why do you think this is?
-
 data DirtyExpr
-  = DirtyEquals    DirtyExpr DirtyExpr
-  | DirtyAdd       DirtyExpr DirtyExpr
-  | DirtyIf        DirtyExpr DirtyExpr DirtyExpr
-  | DirtyIntValue  Int
+  = DirtyEquals DirtyExpr DirtyExpr
+  | DirtyAdd DirtyExpr DirtyExpr
+  | DirtyIf DirtyExpr DirtyExpr DirtyExpr
+  | DirtyIntValue Int
   | DirtyBoolValue Bool
 
 parse :: DirtyExpr -> Maybe (Expr Int)
-parse = error "Implement me"
+parse (DirtyBoolValue _) = Nothing
+parse (DirtyIntValue i) = IntValue <$> Just i
+parse (DirtyIf a x y) = If <$> parseBool a <*> parse x <*> parse y
+parse (DirtyAdd x y) = Add <$> parse x <*> parse y
+parse (DirtyEquals _ _) = Nothing
+
+parseBool :: DirtyExpr -> Maybe (Expr Bool)
+parseBool (DirtyBoolValue b) = BoolValue <$> Just b
+parseBool (DirtyIntValue _) = Nothing
+parseBool (DirtyIf a x y) = If <$> parseBool a <*> parseBool x <*> parseBool y
+parseBool (DirtyAdd x y) = Nothing
+parseBool (DirtyEquals x y) = Equals <$> parse x <*> parse y
 
 -- | c. Can we add functions to our 'Expr' language? If not, why not? What
 -- other constructs would we need to add? Could we still avoid 'Maybe' in the
 -- 'eval' function?
-
-
-
-
 
 {- TEN -}
 
@@ -300,15 +322,14 @@ parse = error "Implement me"
 
 -- | a. Fix that for me - write a list that allows me to hold any functions as
 -- long as the input of one lines up with the output of the next.
-
 data TypeAlignedList a b where
-  -- ...
+  TANil :: TypeAlignedList a a
+  TACons :: (a -> b) -> TypeAlignedList b c -> TypeAlignedList a c
 
 -- | b. Which types are existential?
 
 -- | c. Write a function to append type-aligned lists. This is almost certainly
 -- not as difficult as you'd initially think.
-
 composeTALs :: TypeAlignedList b c -> TypeAlignedList a b -> TypeAlignedList a c
-composeTALs = error "Implement me, and then celebrate!"
-
+composeTALs xs TANil = xs
+composeTALs xs (TACons y ys) = TACons y (composeTALs xs ys)
--- 04-DataKinds/src/Exercises.hs
+++ 04-DataKinds/src/Exercises.hs
@@ -1,39 +1,47 @@
-{-# LANGUAGE DataKinds      #-}
-{-# LANGUAGE GADTs          #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TypeOperators #-}
+
 module Exercises where
 
-import Data.Kind (Type)
-import Data.Function ((&))
-
-
-
-
+import Data.Function (on, (&))
+import Data.Kind (Constraint, Type)
 
 {- ONE -}
 
 -- | One of the restrictions around classes that we occasionally hit is that we
 -- can only have one instance for a type. There are, for example, two good
 -- candidates for a monoid instance when we think about 'Integer':
-
 data IntegerMonoid = Sum | Product
 
 -- | a. Write a newtype around 'Integer' that lets us choose which instance we
 -- want.
+newtype MNum (a :: IntegerMonoid) = MNum {getInt :: Integer}
+  deriving (Show)
 
 -- | b. Write the two monoid instances for 'Integer'.
+instance Semigroup (MNum 'Sum) where
+  a <> b = MNum (getInt a + getInt b)
+
+instance Semigroup (MNum 'Product) where
+  a <> b = MNum (getInt a * getInt b)
+
+instance Monoid (MNum 'Sum) where
+  mempty = MNum 0
+
+instance Monoid (MNum 'Product) where
+  mempty = MNum 1
 
 -- | c. Why do we need @FlexibleInstances@ to do this?
-
-
-
-
 
 {- TWO -}
 
 -- | We can write a type that /is/ of kind 'Type', but has no value-level
 -- members. We usually call this type 'Void':
-
 data Void -- No constructors!
 
 -- | a. If we promote this with DataKinds, can we produce any /types/ of kind
@@ -44,90 +52,91 @@ data Void -- No constructors!
 -- | c. Considering 'Maybe Void', and similar examples of kinds such as
 -- 'Either Void Bool', why do you think 'Void' might be a useful kind?
 
-
-
-
-
 {- THREE -}
 
 -- | a. Write a GADT that holds strings or integers, and keeps track of how
 -- many strings are present. Note that you might need more than 'Nil' and
 -- 'Cons' this time...
-
 data Nat = Z | S Nat
 
-data StringAndIntList (stringCount :: Nat) where
-  -- ...
+data StringAndIntList (stringCount :: Nat) (intCount :: Nat) (totalCount :: Nat) where
+  SINil :: StringAndIntList Z Z Z
+  SCons :: String -> StringAndIntList s i t -> StringAndIntList (S s) i (S t)
+  ICons :: Int -> StringAndIntList s i t -> StringAndIntList s (S i) (S t)
 
 -- | b. Update it to keep track of the count of strings /and/ integers.
 
 -- | c. What would be the type of the 'head' function?
-
-
-
-
+head :: StringAndIntList s i (S t) -> Either String Int
+head (SCons s _) = Left s
+head (ICons i _) = Right i
 
 {- FOUR -}
 
 -- | When we talked about GADTs, we discussed existentials, and how we could
 -- only know something about our value if the context told us:
-
 data Showable where
-  Showable :: Show a => a -> Showable
+  Showable :: (Show a) => a -> Showable
 
 -- | a. Write a GADT that holds something that may or may not be showable, and
 -- stores this fact in the type-level.
-
 data MaybeShowable (isShowable :: Bool) where
-  -- ...
+  IsShowable :: (Show a) => a -> MaybeShowable 'True
+  NotShowable :: a -> MaybeShowable 'False
+
+-- ...
 
 -- | b. Write a 'Show' instance for 'MaybeShowable'. Your instance should not
 -- work unless the type is actually 'show'able.
+instance Show (MaybeShowable 'True) where
+  show (IsShowable a) = show a
 
 -- | c. What if we wanted to generalise this to @Constrainable@, such that it
 -- would work for any user-supplied constraint of kind 'Constraint'? How would
 -- the type change? What would the constructor look like? Try to build this
 -- type - GHC should tell you exactly which extension you're missing.
-
-
-
-
+data Constrainable (c :: Type -> Constraint) where
+  Constrainable :: (c a) => a -> Constrainable c
 
 {- FIVE -}
 
 -- | Recall our list type:
-
 data List a = Nil | Cons a (List a)
 
 -- | a. Use this to write a better 'HList' type than we had in the @GADTs@
 -- exercise. Bear in mind that, at the type-level, 'Nil' and 'Cons' should be
 -- "ticked". Remember also that, at the type-level, there's nothing weird about
 -- having a list of types!
-
 data HList (types :: List Type) where
-  -- HNil  :: ...
-  -- HCons :: ...
+  HNil :: HList Nil
+  HCons :: x -> HList xs -> HList (Cons x xs)
 
 -- | b. Write a well-typed, 'Maybe'-less implementation for the 'tail' function
 -- on 'HList'.
+tail :: HList (Cons x xs) -> HList xs
+tail (HCons _ xs) = xs
 
 -- | c. Could we write the 'take' function? What would its type be? What would
 -- get in our way?
+type family Take (n :: Nat) (xs :: List Type) :: List Type where
+  Take Z xs = xs
+  Take _ Nil = Nil
+  Take (S n) (Cons x xs) = Cons x (Take n xs)
 
-
-
-
-
+take :: SNat n -> HList xs -> HList (Take n xs)
+take SZ xs = xs
+take _ HNil = HNil
+take (SS n) (HCons x xs) = HCons x (Exercises.take n xs)
 {- SIX -}
 
 -- | Here's a boring data type:
+data BlogAction (privilege :: [Role]) where
+  AddBlog :: BlogAction '[Admin, Moderator, User]
+  DeleteBlog :: BlogAction '[Admin]
+  AddComment :: BlogAction '[Admin, Moderator, User]
+  DeleteComment :: BlogAction '[Admin, Moderator]
 
-data BlogAction
-  = AddBlog
-  | DeleteBlog
-  | AddComment
-  | DeleteComment
-
+data Role = Admin | Moderator | User
 -- | a. Two of these actions, 'DeleteBlog' and 'DeleteComment', should be
 -- admin-only. Extend the 'BlogAction' type (perhaps with a GADT...) to
 -- express, at the type-level, whether the value is an admin-only operation.
@@ -145,10 +154,6 @@ data BlogAction
 -- the three roles at the type-level, and modify our type to keep track of
 -- this?
 
-
-
-
-
 {- SEVEN -}
 
 -- | When we start thinking about type-level Haskell, we inevitably end up
@@ -161,47 +166,44 @@ data BlogAction
 
 data SBool (value :: Bool) where
   SFalse :: SBool 'False
-  STrue  :: SBool 'True
+  STrue :: SBool 'True
 
 -- | a. Write a singleton type for natural numbers:
-
 data SNat (value :: Nat) where
-  -- ...
+  SZ :: SNat Z
+  SS :: SNat n -> SNat (S n)
+
+-- ...
 
 -- | b. Write a function that extracts a vector's length at the type level:
-
 length :: Vector n a -> SNat n
 length = error "Implement me!"
 
 -- | c. Is 'Proxy' a singleton type?
-
 data Proxy a = Proxy
-
-
-
-
 
 {- EIGHT -}
 
 -- | Let's imagine we're writing some Industry Haskell™, and we need to read
 -- and write to a file. To do this, we might write a data type to express our
 -- intentions:
-
-data Program                     result
-  = OpenFile            (Program result)
-  | WriteFile  String   (Program result)
-  | ReadFile  (String -> Program result)
-  | CloseFile (          Program result)
-  | Exit                         result
+data Program result
+  = OpenFile (Program result)
+  | WriteFile String (Program result)
+  | ReadFile (String -> Program result)
+  | CloseFile (Program result)
+  | Exit result
 
 -- | We could then write a program like this to use our language:
-
 myApp :: Program Bool
-myApp
-  = OpenFile $ WriteFile "HEY" $ (ReadFile $ \contents ->
-      if contents == "WHAT"
-        then WriteFile "... bug?" $ Exit False
-        else CloseFile            $ Exit True)
+myApp =
+  OpenFile $
+    WriteFile "HEY" $
+      ( ReadFile $ \contents ->
+          if contents == "WHAT"
+            then WriteFile "... bug?" $ Exit False
+            else CloseFile $ Exit True
+      )
 
 -- | ... but wait, there's a bug! If the contents of the file equal "WHAT", we
 -- forget to close the file! Ideally, we would like the compiler to help us: we
@@ -225,20 +227,14 @@ myApp
 
 -- | EXTRA: write an interpreter for this program. Nothing to do with data
 -- kinds, but a nice little problem.
-
 interpret :: Program {- ??? -} a -> IO a
 interpret = error "Implement me?"
-
-
-
-
 
 {- NINE -}
 
 -- | Recall our vector type:
-
 data Vector (n :: Nat) (a :: Type) where
-  VNil  :: Vector 'Z a
+  VNil :: Vector 'Z a
   VCons :: a -> Vector n a -> Vector ('S n) a
 
 -- | Imagine we want to write the '(!!)' function for this vector. If we wanted
@@ -247,12 +243,11 @@ data Vector (n :: Nat) (a :: Type) where
 
 -- | a. Implement this type! This might seem scary at first, but break it down
 -- into Z and S cases. That's all the hint you need :)
+data SmallerThan (limit :: Nat)
 
-data SmallerThan (limit :: Nat) where
-  -- ...
+-- ...
 
 -- | b. Write the '(!!)' function:
-
 (!!) :: Vector n a -> SmallerThan n -> a
 (!!) = error "Implement me!"
 
